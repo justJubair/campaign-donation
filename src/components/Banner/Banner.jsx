2ded25d422a4ffecca1387ae5407850ba00d0b7c
Jubair <jubair.ahmed2838@gmail.com>
2023-09-25 00:50:17 +0600

    Add: navbar responsive

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,11 +2,11 @@ import './Banner.css'
 
 const Banner = () => {
   return (
-    <div className='flex flex-col items-center justify-center banner md:h-[80vh]'>
+    <div className='flex flex-col items-center justify-center banner h-[100vh] -mt-32'>
       <h1 className="text-4xl font-bold text-center">I Grow By Helping People In Need</h1>
     <div>
-        <form className='flex items-center mt-5'>
-            <input className='w-96 py-2 px-2 rounded-lg' type="text" name="search" id="search" placeholder="Search here..."/>
+        <form className='flex items-center mt-8'>
+            <input className='md:w-96 py-2 px-2 rounded-lg' type="text" name="search" id="search" placeholder="Search here..."/>
             <input className='btn bg-red-400 border-none text-white ml-4 hover:bg-red-700' type="submit" value="Search" />
         </form>
     </div>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,60 @@
-import { NavLink } from 'react-router-dom'
-import img from '../../assets/Logo.png'
-import './Nav.css'
+import { NavLink } from "react-router-dom";
+import img from "../../assets/Logo.png";
+import "./Nav.css";
 const Nav = () => {
+  const links = (
+    <>
+      <li>
+     
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+      
+        <NavLink to="/donation">Donation</NavLink>
+      </li>
+      <li>
+     
+        <NavLink to="/statistics">Statistics</NavLink>
+      </li>
+    </>
+  );
   return (
-    <div className='max-w-screen-xl mx-auto p-4 flex justify-between items-center bg-transparent'>
-    <span className='w-40'>  <img src={img} alt="" /></span>
-      <ul className='flex gap-6 font-semibold'>
-       <li> <NavLink to='/'>Home</NavLink></li>
-       <li> <NavLink to='/donation'>Donation</NavLink></li>
-       <li> <NavLink to='/statistics'>Statistics</NavLink></li>
-      </ul>
+    <div className="flex items-center justify-between bg-base-100 max-w-screen-xl mx-auto p-4">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+           {links}
+          </ul>
+        </div>
+        <img className="w-40 " src={img} alt="" />
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 font-bold">
+        {links}
+        </ul>
+      </div>
+     
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
