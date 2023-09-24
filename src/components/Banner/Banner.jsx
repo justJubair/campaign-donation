6d9d5bb53399ab91b53f8ea7516f8ad966ea877a
Jubair <jubair.ahmed2838@gmail.com>
2023-09-25 00:25:31 +0600

    Add: banner page

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,11 +3,11 @@ import './Banner.css'
 const Banner = () => {
   return (
     <div className='flex flex-col items-center justify-center banner md:h-[80vh]'>
-      <h1 className="text-2xl font-bold text-center">I Grow By Helping People In Need</h1>
+      <h1 className="text-4xl font-bold text-center">I Grow By Helping People In Need</h1>
     <div>
-        <form>
-            <input type="text" name="search" id="search" placeholder="Search here..."/>
-            <input type="submit" value="Search" />
+        <form className='flex items-center mt-5'>
+            <input className='w-96 py-2 px-2 rounded-lg' type="text" name="search" id="search" placeholder="Search here..."/>
+            <input className='btn bg-red-400 border-none text-white ml-4 hover:bg-red-700' type="submit" value="Search" />
         </form>
     </div>
     </div>
