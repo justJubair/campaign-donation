fb63a9c27af40eecd975f460cec9166bbcffd311
Jubair <jubair.ahmed2838@gmail.com>
2023-09-25 19:22:48 +0600

    Add: see all button implemented

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -8,15 +8,25 @@ const Donation = () => {
 
   const data = useLoaderData();
  const [donatedCards, setDonatedCards] = useState([])
+ const [displayCardsLength, setDisplayCardsLength] = useState(4)
+ const [isShowAll, setIsShowAll] = useState(false)
   useEffect(()=>{
     const storedDonationId = getDonationId()
-    const donatedCards = data.filter(donationCard=> storedDonationId.includes(donationCard.id))
-    setDonatedCards(donatedCards)
+    const cardsDonated = data.filter(donationCard=> storedDonationId.includes(donationCard.id))
+    setDonatedCards(cardsDonated)
   },[data])
- 
+ const handleShowAll =()=>{
+    setDisplayCardsLength(donatedCards.length)
+    setIsShowAll(true)
+ }
   return (
-    <div className="max-w-screen-xl mx-auto p-4 mt-2 flex justify-center gap-8 flex-wrap md:flex-nowrap">
-      {donatedCards.map(donatedCard=> <DonationCard key={donatedCard.id} donatedCard={donatedCard}/>)}
+    <div className="max-w-screen-xl mx-auto p-4 mt-2 ">
+     <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+     {donatedCards.slice(0, displayCardsLength).map(donatedCard=> <DonationCard key={donatedCard.id} donatedCard={donatedCard}/>)}
+     </div>
+    <div className="flex justify-center my-8">
+    <button onClick={handleShowAll} className={`btn bg-lime-800 text-white ${donatedCards.length>4 && !isShowAll ? 'block' : 'hidden'}`}>See all</button>
+    </div>
     </div>
   )
 }
--- src/pages/Donation/DonationCard.jsx
+++ src/pages/Donation/DonationCard.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
 
-
 // eslint-disable-next-line react/prop-types
 const DonationCard = ({donatedCard}) => {
     const {
+     
         price,
         category,
         categoryBackgroundColor,
@@ -12,7 +12,7 @@ const DonationCard = ({donatedCard}) => {
         textColor,
         title,
       } = donatedCard;
-
+     
     const cardBackground={
         backgroundColor: cardColor
     }
@@ -26,6 +26,7 @@ const DonationCard = ({donatedCard}) => {
     const btnBackground ={
         backgroundColor: textColor
     }
+   
   return (
     <div className="flex flex-col items-center gap-8 p-4 rounded-lg md:flex-row" style={cardBackground}>
       <div>
@@ -35,7 +36,7 @@ const DonationCard = ({donatedCard}) => {
       <span style={categoryColor} className="badge font-bold p-3">{category}</span>
       <h3 className="text-2xl font-bold my-2">{title}</h3>
       <p className="text-xl font-bold" style={priceColor}>${price.toFixed(2)}</p>
-      <button style={btnBackground} className="btn text-white mt-4">View Details</button>
+      <button  style={btnBackground} className="btn text-white mt-4">View Details</button>
       </div>
     </div>
   )
