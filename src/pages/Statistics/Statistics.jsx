837c89cf2742435ba8f611dd34072828b5f8bd3e
Jubair <jubair.ahmed2838@gmail.com>
2023-09-25 21:00:08 +0600

    Add: pie chart added

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,11 +1,72 @@
+import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
 
+import { getDonationId } from "../../utils/localStorage";
 
 const Statistics = () => {
-  return (
-    <div>
-      <h1>statistics</h1>
-    </div>
-  )
-}
+  const totalDonatedCards = getDonationId();
+  const totalNumberDonations = totalDonatedCards.length;
 
-export default Statistics
+  const percentageOfDonations = parseFloat(
+    ((totalNumberDonations / 12) * 100).toFixed(1)
+  );
+  const percentageOfAvailable = 100 - percentageOfDonations;
+
+  const data01 = [
+    {
+      name: "Total Donation",
+      value: percentageOfAvailable,
+    },
+    {
+      name: "Your Donation",
+      value: percentageOfDonations,
+    },
+  ];
+  const COLORS = ["#0088FE", "#00C49F"];
+  const yourDonationColor = {
+    backgroundColor: "#00C49F",
+  };
+  const totalDonationColor = {
+    backgroundColor: "#0088FE",
+  };
+  return (
+    <div className=" max-w-screen-xl mx-auto p-4 mt-2">
+      <div>
+        <ResponsiveContainer width="100%" height={500}>
+          <PieChart >
+            <Pie
+              dataKey="value"
+              isAnimationActive={false}
+              data={data01}
+              cx="50%"
+              cy="50%"
+              outerRadius={180}
+              fill="#8884d8"
+              label
+            >
+              {data01.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+
+            <Tooltip />
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+      <div className="flex justify-center gap-10">
+        <div className="flex gap-2">
+          <p className="font-semibold">Your Donation</p>
+          <span className="px-10" style={yourDonationColor}></span>
+        </div>
+        <div className="flex gap-2">
+          <p className="font-semibold">Total Donation</p>
+          <span className="px-10" style={totalDonationColor}></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Statistics;
